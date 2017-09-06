caa59bfa6cf7c1943c40597dd9f19ba4976fafbd
Shubham Chawla <shchawla@microsoft.com>
2017-09-06 14:31:46 +0530

    js changes

--- jssource.js
+++ jssource.js
@@ -33849,7 +33849,6 @@ background: url(\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAACCAYAAACZ
         };
 
         window.renderCard = function(stringJson) {
-          return stringJson;
                 console.log("Received payload:", stringJson);
                 var parsedJSON = JSON.parse(stringJson);
                 console.log("Parsed JSON: ", parsedJSON);
