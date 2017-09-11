4dca07472ecb52967729d9086aa1ff2c0bcd1f1a
Shubham Chawla <shchawla@microsoft.com>
2017-09-11 19:45:48 +0530

    jstest chnages

--- jssource.js
+++ jssource.js
@@ -33882,7 +33882,7 @@ background: url(\"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAACCAYAAACZ
 
           computeMessageCardHash(stringJson, function(result) {
            console.log("Got message card hash as:", result);
-           return result
+           var x = result;
        }, function(err){
            console.log("Error generating message card hash");
        })
