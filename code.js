765aa98ffbebd02d77789d5cd06a87e14f66f627
Shubham Chawla <shchawla@microsoft.com>
2017-11-30 21:09:26 +0530

    js chnage

--- code.js
+++ code.js
@@ -174,14 +174,32 @@ function onExecuteAction(action) {
 }
 
 function showPopupCard(action) {
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", "/showActionPopUp", true);
-	xhr.setRequestHeader('Content-Type', 'application/json');
-	xhr.send(JSON.stringify({
-    	head:  '<link rel="stylesheet" type="text/css" href="http://adaptivecards.io/visualizer/css/app.css">  <link rel="stylesheet" type="text/css" href="http://adaptivecards.io/visualizer/css/teams.css">',
-    	body: action.card.render()
-	}));}
-	
+	post('www.google.com/showActionPopUp', {head: '<link rel="stylesheet" type="text/css" href="http://adaptivecards.io/visualizer/css/app.css">  <link rel="stylesheet" type="text/css" href="http://adaptivecards.io/visualizer/css/teams.css">', body: action.card.render()});
+}
+
+function post(path, params, method) {
+    method = method || "post"; // Set method to post by default if not specified.
+
+    // The rest of this code assumes you are not using a library.
+    // It can be made less wordy if you use one.
+    var form = document.createElement("form");
+    form.setAttribute("method", method);
+    form.setAttribute("action", path);
+
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+        }
+    }
+
+    document.body.appendChild(form);
+    form.submit();
+}
 
 function MessageCard() {
     this.style = "default";
